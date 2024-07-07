27b5f77f1fa1e4286368ed455fefad36941d7419
AakashHubGit <aakash.pandey@somaiya.edu>
2024-07-07 19:06:12 +0530

    db.js updated

--- backend/db.js
+++ backend/db.js
@@ -14,9 +14,3 @@ const connectToMongo = async () => {
 };
 
 module.exports = connectToMongo;
-
-// MONGO_URI= mongodb+srv://teamprojects2902:startup@cluster0.wccp0eu.mongodb.net/turfit?retryWrites=true&w=majority&appName=Cluster0
-// BASE_URL = http://localhost:3000
-// JWT_SECRET= BookTurfFromTurfit
-// TWILIO_SID= ACdf0e2267b88a3eec8cf294c267bd2dbf
-// TWILIO_AUTH= 6003ae468973c7545e4feacff38a761d
